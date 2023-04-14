a5598839e295020556adf79d5673cb735483d48c
Benjamin Luo <benjaminpluo2001@gmail.com>
2023-04-13 19:07:55 -0700

    assign6

--- assign6/wordalign.py
+++ assign6/wordalign.py
@@ -1,10 +1,10 @@
-english_sentences = "data/class_test.en"
-foreign_sentences = "data/class_test.es"
+english_sentences = "assign6/data/class_test.en"
+foreign_sentences = "assign6/data/class_test.es"
 iterations = 1
 probability_threshold = 0.0
 
-pairs = []
-count = {}
+
+
 prob = {}
 
 # open english sentences 
@@ -14,6 +14,7 @@ eng_sent = open(english_sentences, encoding='utf-8')
 forn_sent = open(foreign_sentences, encoding='utf-8')
 
 for i in range(iterations):
+    count = {}
     while True:
         eline = eng_sent.readline()
         fline = forn_sent.readline()
@@ -23,36 +24,49 @@ for i in range(iterations):
 
         englishSplitLine = eline.split()
         foreignSplitLine = fline.split()
-        # array of dictionaries so each iteration can calculate pairs seperately!
-        pairs[i] = {}
 
+        
+        # array of dictionaries so each iteration can calculate pairs seperately!
+        # pairs[i] = {}
+        # adding a dictionary into the array every iteration
+        
         for e in englishSplitLine:
             for f in foreignSplitLine:
+                if not f in prob:
+                    # if the given f word is not in prob, add it
+                    prob[f] = {}
+                    if not e in prob[f]:
+                        prob[f][e] = 0.01
 
 
-                temp_prob = prob
+        for e in englishSplitLine:
+            pairs = {}
+            for f in foreignSplitLine:
+                # check if f in pairs
+                # if it is, ive already calculated the sum, just say pair_demoninator = pairs[f]
+                # 
+                if f in pairs:
+                    pair_denominator = pairs[f]
 
+                # temp_prob = prob
+                else:
                 # calculate the denominator of the pair
-                pair_denominator = 0.0
-                for e2 in englishSplitLine:
-                    if not f in prob:
-                        prob[f] = {}
-                    if not e2 in prob[f]:
-                        prob[f][e2] = 1
-                    pair_denominator += prob[f][e2]
-                
-                if not f in pairs:
-                    pairs[f] = {}
+                    pair_denominator = 0.0
+                    for e2 in englishSplitLine:
+                        pair_denominator += prob[f][e2]
+                    
+                    if not f in pairs:
+                        pairs[f] = {}
 
-                pairs[f][e] = prob[f][e] / pair_denominator
+                    pairs[f] = pair_denominator
 
                 if not e in count:
                     count[e] = {"<COUNT>" : 0}
                 if not f in count[e]:
                     count[e][f] = 0
 
-                count[e][f] += pairs[f][e]
-                count[e]["<COUNT>"] += pairs[f][e]
+                count[e][f] += prob[f][e]/pairs[f]
+                count[e]["<COUNT>"] += prob[f][e]/pairs[f]
         
         for e in englishSplitLine:
             for f in foreignSplitLine:
